1ab024e9d82b630392512d0cd7e528649e5ce3bc
dannegm <dannegm@gmail.com>
2018-09-27 11:37:50 -0500

    Firebase Function to send emails

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,5 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.sendEmail = functions.https.onCall ((data, context) => {
     const mailgun = require ('mailgun-js') ({
         apiKey: functions.config().mailgun.api_key,
